c363d45ea36c99c08302e5a8984b6a0cb44945b0
Ozzyz <aasmundbrekke@gmail.com>
2017-09-04 23:08:43 +0200

    Remove unused code

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,83 +1,36 @@
+import matplotlib.cm as cm
 import functools
+import numpy as np
 import matplotlib.pyplot as plt
 
+def mand_zoom():
+    N = 4096
+    min_x, max_x = -2.5 , 1
+    min_y, max_y = -1, 1
+    X = np.linspace(min_x, max_x, N)
+    Y = np.linspace(min_y, max_y, N)
+    Z = np.zeros((X.size, Y.size), dtype=np.complex_)
+    max_len = 4
+    max_iters = 128
+    Iters = np.zeros((X.size, Y.size), dtype=np.int)
+    for x in range(X.size):
+        for y in range(Y.size):
+            num_iters = 0
+            c = X[x] + 1j*Y[y]
+            Z[x][y] = c
+            while(abs(Z[x][y]) < max_len and num_iters < max_iters):
+                Z[x][y] = Z[x][y]**2 + c
+                num_iters += 1
+            plt.scatter(X[x], Y[y], cm.hot(num_iters/max_iters))
+            Iters[x][y] = num_iters
+            print("(%s, %s) Num iters: %s" % (x, y, num_iters))
+    #xx, yy = np.meshgrid(X,Y)
+    #Iters = [cm.hot(x/max_iters) for x in Iters.flatten('F')]
 
-class ComplexNumber:
-
-    def __init__(self, real, imaginary):
-        self.real = real
-        self.imaginary = imaginary
-
-    def __mul__(self, other):
-        # (a+ib)(c+id) = ac-bd+i(ad+bc)
-        real_part = self.real*other.real - self.imaginary*other.imaginary
-        imaginary_part = self.real*other.imaginary + self.imaginary*other.real
-        return ComplexNumber(real_part, imaginary_part)
-
-    def __add__(self, other):
-        if(isinstance(other, ComplexNumber)):
-            return ComplexNumber(self.real+other.real, self.imaginary + other.imaginary)
-        if(isinstance(other, float) or isinstance(other, int)):
-            return ComplexNumber(self.real + other, self.imaginary)
-        raise ValueError('Illegal addition to complexnumber!')
-
-    def __eq__(self, other):
-        return (self.real, self.imaginary) == (other.real, other.imaginary)
-
-    # This is needed in order to use functools.lru_cache
-    def __hash__(self):
-        return hash((self.real, self.imaginary))
-
-    def __len__(self):
-        return float(self.real**2 + self.imaginary**2)
-
-    def __repr__(self):
-        return "%s %si" % (self.real, self.imaginary)
-
-    
-@functools.lru_cache(maxsize=1024)
-def mandelbrot(z, c):
-    return z*z + c
-
-def mandelbrot_seq(x,y, max_iter, max_len, max_x, max_y):
-    # The number of iterations a number go through decide the pixel intensity
-    num_iters = 0
-    # TODO: Scale numbers to match mandelbrot range
-    # X : (-2.5,1)
-    # Y: (-1,1)
-    x = (((x-max_x*0.5)*(3.5/(max_x*0.5)))-1.5)/2
-    y = (y-(max_y*0.5))/(max_y*0.5)
-    z_n = ComplexNumber(0,0)
-    c = ComplexNumber(x,y)
-    
-    while(True): # and num_iters <= max_iter):
-        z_n = mandelbrot(z_n, c)
-        num_iters += 1
-        if(num_iters > max_iter or len(z_n) > max_len):
-            break
-    return num_iters
-
-
-
-def plot():
-     
-    plt.figure()
-    Z = []
-    X = []
-    Y = []
-    max_x = 1<<13
-    max_y = 1<<13
-    for x in range(max_x):
-        for y in range(max_y):
-            res = mandelbrot_seq(x,y, 255, 2, max_x, max_y)
-            X.append(x)
-            Y.append(y)
-            Z.append(res)
-        print("(%s, %s) Num iters: %s " % (x,y,res))
-    Z = [(1-x/255) for x in Z]
-    plt.scatter(X, Y, c=Z)
-    plt.savefig("mandelbrot.png")
+    #plt.xlim((min_x, max_x))
+    #plt.ylim((min_y, max_y))
+    #plt.scatter(xx,yy, c=Iters)
+    plt.savefig('testmandelbrot_255iters')
     plt.show()
-
-
-plot()
+    
+mand_zoom()
