ae8eee477a9dcbccf1d3bbf1bda95409319b97f5
Ozzyz <aasmundbrekke@gmail.com>
2017-09-05 02:05:44 +0200

    Add 5k image, cleaned code

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,36 +1,44 @@
 import matplotlib.cm as cm
-import functools
 import numpy as np
 import matplotlib.pyplot as plt
-
-def mand_zoom():
-    N = 4096
-    min_x, max_x = -2.5 , 1
-    min_y, max_y = -1, 1
-    X = np.linspace(min_x, max_x, N)
-    Y = np.linspace(min_y, max_y, N)
+import time
+from PIL import Image
+def mandelbrot(X, Y, max_iters, max_len, N):
+    img = Image.new('RGBA', (N, N))
+    pixels = img.load()
+    
     Z = np.zeros((X.size, Y.size), dtype=np.complex_)
+    
+    for i in range(X.size):
+        for j in range(Y.size):
+            num_iters = 0
+            c = X[i] + 1j*Y[j]
+            Z[i][j] = c
+            while(abs(Z[i][j]) < max_len and num_iters < max_iters):
+                Z[i][j] = Z[i][j]**2 + c
+                num_iters += 1
+            rgba_arr = cm.hot(num_iters/max_iters)
+            # Since PIL wants rgba from 0 to 255
+            pixels[i,j] = tuple(int(x*255) for x in rgba_arr)
+        print("(%s, %s) Num iters: %s" % (i, j, num_iters))
+    return img
+    
+def main():
+    # Width and height in pixels
+    num_pixels = 3000
+    min_x, max_x = -1.5 , 1
+    min_y, max_y = -1, 1
+    X = np.linspace(min_x, max_x, num_pixels)
+    Y = np.linspace(min_y, max_y, num_pixels)
     max_len = 4
     max_iters = 128
-    Iters = np.zeros((X.size, Y.size), dtype=np.int)
-    for x in range(X.size):
-        for y in range(Y.size):
-            num_iters = 0
-            c = X[x] + 1j*Y[y]
-            Z[x][y] = c
-            while(abs(Z[x][y]) < max_len and num_iters < max_iters):
-                Z[x][y] = Z[x][y]**2 + c
-                num_iters += 1
-            plt.scatter(X[x], Y[y], cm.hot(num_iters/max_iters))
-            Iters[x][y] = num_iters
-            print("(%s, %s) Num iters: %s" % (x, y, num_iters))
-    #xx, yy = np.meshgrid(X,Y)
-    #Iters = [cm.hot(x/max_iters) for x in Iters.flatten('F')]
 
-    #plt.xlim((min_x, max_x))
-    #plt.ylim((min_y, max_y))
-    #plt.scatter(xx,yy, c=Iters)
-    plt.savefig('testmandelbrot_255iters')
-    plt.show()
+    start = time.time()
+    img = mandelbrot(X, Y, max_iters, max_len, num_pixels)
+    end = time.time()
+    print("%sx%s sized image finished in %s seconds" % (num_pixels, num_pixels,end-start))
+    img.show()
+    img.save("mandelbrot%sx%s.png" % (num_pixels, num_pixels))
     
-mand_zoom()
+
+main()
