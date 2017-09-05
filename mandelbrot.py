4c82178c8c77a6d8ced3547db75ec37bbd008ad6
Ozzyz <aasmundbrekke@gmail.com>
2017-09-05 14:57:16 +0200

    Cleaned up code

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,42 +3,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 from PIL import Image
-def mandelbrot(X, Y, max_iters, max_len, N):
-    img = Image.new('RGBA', (N, N))
-    pixels = img.load()
-    
-    Z = np.zeros((X.size, Y.size), dtype=np.complex_)
-    
+
+from numba import jit
+
+@jit
+def mandelbrot(X, Y, max_iters, max_len):
+
+    iters = np.zeros((X.size, Y.size), dtype=np.uint)
+
     for i in range(X.size):
         for j in range(Y.size):
             num_iters = 0
             c = X[i] + 1j*Y[j]
-            Z[i][j] = c
-            while(abs(Z[i][j]) < max_len and num_iters < max_iters):
-                Z[i][j] = Z[i][j]**2 + c
+            z = c
+            while(abs(z) < max_len and num_iters < max_iters):
+                z = z**2 + c
                 num_iters += 1
-            rgba_arr = cm.hot(num_iters/max_iters)
-            # Since PIL wants rgba from 0 to 255
-            pixels[i,j] = tuple(int(x*255) for x in rgba_arr)
-        print("(%s, %s) Num iters: %s" % (i, j, num_iters))
-    return img
-    
+            iters[i, j] = num_iters
+        print(i,',',j)
+    return iters
+
+
 def main():
     # Width and height in pixels
-    num_pixels = 3000
-    min_x, max_x = -1.5 , 1
-    min_y, max_y = -1, 1
+    num_pixels = 7500
+    min_x, max_x = 0, 0.002
+    min_y, max_y = -0.823, -0.821
     X = np.linspace(min_x, max_x, num_pixels)
     Y = np.linspace(min_y, max_y, num_pixels)
     max_len = 4
-    max_iters = 128
+    max_iters = 255
 
     start = time.time()
-    img = mandelbrot(X, Y, max_iters, max_len, num_pixels)
+    iters = mandelbrot(X, Y, max_iters, max_len) 
     end = time.time()
     print("%sx%s sized image finished in %s seconds" % (num_pixels, num_pixels,end-start))
-    img.show()
-    img.save("mandelbrot%sx%s.png" % (num_pixels, num_pixels))
-    
+    #plt.axis('off')
+    #plt.imshow(iters, cmap=cm.hot)
+    plt.imsave('mandelbrot_zoomed2.png', iters)
+    #plt.show()
+
 
 main()
